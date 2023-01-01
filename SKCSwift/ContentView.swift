ea8059c0155572cdfc0be6ca42185c78d46c8433
Javi <rtomyj@gmail.com>
2023-01-01 16:59:44 -0600

    Added structs that will be used to read json output from skc api

--- SKCSwift/ContentView.swift
+++ SKCSwift/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-let cardId = "40044918"
+let cardId = "90307498"
 let imageUrl = URL(string: "https://images.thesupremekingscastle.com/cards/lg/\(cardId).jpg")
 
 func getCardData(string: String)->  String {
@@ -15,20 +15,25 @@ func getCardData(string: String)->  String {
     request.httpMethod = "GET"
     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
     
+//    var cardData: SKCCardInfoOutput
+    
     let session = URLSession.shared
     let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
-        print(response!)
-        do {
-            let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
-            print(json)
-        } catch {
-            print("error")
+        if (error != nil) {
+            print("Error occurred while calling SKC API \(error!.localizedDescription)")
+        } else {
+            do {
+                let decoder = JSONDecoder()
+                let cardData = try decoder.decode(SKCCardInfoOutput.self, from: data!)
+            } catch {
+                print("An error ocurred while decoding output from SKC API \(error.localizedDescription)")
+            }
         }
     })
     
     task.resume()
     
-    return "YOOOO"
+    return "xxx"
 }
 
 struct CardInfo: View {
@@ -37,16 +42,32 @@ struct CardInfo: View {
         VStack {
             Text(getCardData(string: cardId))
                 .font(.title)
+                .multilineTextAlignment(.leading)
                 .bold()
             AsyncImage(url: imageUrl)
-                .cornerRadius(100.0)
-//                .aspectRatio(contentMode: .fill)
+                .cornerRadius(50.0)
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct CardInfo_Previews: PreviewProvider {
     static var previews: some View {
         CardInfo()
     }
+}
+
+struct SKCMonsterAssociation: Codable {
+    var level: Int?
+}
+
+struct SKCCardInfoOutput: Codable {
+    var cardID: String
+    var cardName: String
+    var cardColor: String
+    var cardAttribute: String
+    var cardEffect: String
+    var monsterType: String?
+    var monsterAssociation: SKCMonsterAssociation
+    var monsterAttack: Int?
+    var monsterDefense: Int?
 }
