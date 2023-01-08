624019634e2711afe8318c47bfa3855f3a7aecad
Javi <rtomyj@gmail.com>
2023-01-08 16:14:28 -0600

    Code cleanup

---
+++ SKCSwift/Models/SearchResults.swift
@@ -0,0 +1,14 @@
+//
+//  CardSearchResults.swift
+//  SKCSwift
+//
+//  Created by Javi Gomez on 1/8/23.
+//
+
+import Foundation
+
+struct SearchResults: Identifiable {
+    var id = UUID()
+    var section: String
+    var results: [Card]
+}
---
+++ SKCSwift/ViewModels/CardSearchViewModel.swift
@@ -0,0 +1,123 @@
+//
+//  SearchCardViewModel.swift
+//  SKCSwift
+//
+//  Created by Javi Gomez on 1/5/23.
+//
+
+import SwiftUI
+
+struct CardSearchViewModel: View {
+    @State private var searchResults = [SearchResults]()
+    @State private var searchResultsIds = [String]()
+    @State private var searchText = ""
+    @State private var isFetching = false
+    @State private var task: URLSessionDataTask?
+    
+    func newSearchSubject(value: String) {
+        if (isFetching) {
+            task?.cancel()
+        }
+        
+        if (value == "") {
+            self.searchResults = []
+            self.searchResultsIds = []
+        } else {
+            isFetching = true
+            task = searchCard(searchTerm: value.trimmingCharacters(in: .whitespacesAndNewlines), {result in
+                switch result {
+                case .success(let cards):
+                    var results = [String: [Card]]()
+                    var sections = [String]()
+                    var searchResultsIds = [String]()
+                    
+                    cards.forEach { card in
+                        let section = card.cardColor
+                        if results[section] == nil {
+                            results[section] = []
+                            sections.append(section)
+                        }
+                        results[section]!.append(card)
+                        searchResultsIds.append(card.cardID)
+                    }
+                    
+                    if (self.searchResultsIds.count != searchResultsIds.count || self.searchResultsIds != searchResultsIds) {
+                        var searchResults = [SearchResults]()
+                        for (section) in sections {
+                            searchResults.append(SearchResults(section: section, results: results[section]!))
+                        }
+                        
+                        self.searchResults = searchResults
+                        self.searchResultsIds = searchResultsIds
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+                
+                isFetching = false
+            })
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            if (!isFetching && !searchText.isEmpty && searchResults.isEmpty) {
+                VStack {
+                    Spacer()
+                    Text("Nothing found in database")
+                        .font(.title2)
+                }.padding(.horizontal)
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        minHeight: 0,
+                        maxHeight: .infinity,
+                        alignment: .center
+                    )
+            } else if (searchText.isEmpty) {
+                VStack(alignment: .leading) {
+                    Text("Suggestions")
+                        .font(.title)
+                }.padding(.horizontal)
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        minHeight: 0,
+                        maxHeight: .infinity,
+                        alignment: .topLeading
+                    )
+            }
+            
+            List(searchResults) { sr in
+                Section(header: Text(sr.section).font(.headline).fontWeight(.black) ) {
+                    ForEach(sr.results, id: \.cardID) { card in
+                        LazyVStack {
+                            NavigationLink(destination: CardSearchLinkDestination(cardId: card.cardID), label: {
+                                CardSearchResultViewModel(cardId: card.cardID, cardName: card.cardName, monsterType: card.monsterType)
+                            })
+                        }
+                    }
+                }
+            }.listStyle(.plain)
+                .searchable(text: self.$searchText, prompt: "Search for card...")
+                .onChange(of: searchText, perform: newSearchSubject)
+                .navigationTitle("Search")
+        }
+    }
+}
+
+struct CardSearchLinkDestination: View {
+    var cardId: String
+    
+    var body: some View {
+        CardViewModel(cardId: cardId)
+            .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct SearchCardViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        CardSearchViewModel()
+        CardSearchLinkDestination(cardId: "40044918")
+    }
+}
