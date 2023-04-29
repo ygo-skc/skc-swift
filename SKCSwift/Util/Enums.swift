f768ff30201aa911a81c74ec01eb25bec4a2a3fb
Javi <rtomyj@gmail.com>
2023-04-29 00:18:54 -0500

    Revert "Sync"
    This reverts commit fcb90b4a8f1ee892d0411f6faa21ae73e8be34e2.

---
+++ SKCSwift/Util/Enums.swift
@@ -0,0 +1,16 @@
+//
+//  EnumsFile.swift
+//  SKCSwift
+//
+//  Created by Javi Gomez on 1/18/23.
+//
+
+import Foundation
+
+enum Attribute: String {
+    case dark = "Dark", light = "Light", earth = "Earth", wind = "Wind", water = "Water", fire = "Fire"
+}
+
+enum BanListFormat: String {
+    case tcg = "TCG", md = "Master Duel", dl = "Duel Links"
+}
