b9587f7ec2e4420c1f42e8be632b641d06f07f88
Javi <rtomyj@gmail.com>
2023-05-02 23:52:46 -0500

    Cleanup

--- SKCSwift/Requests/APIURLs.swift
+++ SKCSwift/Requests/APIURLs.swift
@@ -16,12 +16,20 @@ private func createURL(components: URLComponents) -> URL {
 
 // SKC API URL creation methods
 
-func cardInfoURL(cardID: String) -> URL {
+private func baseURLComponents(host: String, path: String) -> URLComponents {
     var components = URLComponents()
     components.scheme = "https"
-    components.host = SKC_API_BASE_URL.description
-    components.path = String(format: SKC_API_CARD_INFORMATION_ENDPOINT.description, cardID)
+    components.host = host
+    components.path = path
     
+    return components
+}
+
+func cardInfoURL(cardID: String) -> URL {
+    var components = baseURLComponents(
+        host: SKC_API_BASE_URL.description,
+        path: String(format: SKC_API_CARD_INFORMATION_ENDPOINT.description, cardID)
+    )
     components.queryItems = [
         URLQueryItem(name: "allInfo", value: "true")
     ]
@@ -30,11 +38,10 @@ func cardInfoURL(cardID: String) -> URL {
 }
 
 func searchCardURL(cardName: String) -> URL {
-    var components = URLComponents()
-    components.scheme = "https"
-    components.host = SKC_API_BASE_URL.description
-    components.path = SKC_API_SEARCH_ENDPOINT.description
-    
+    var components = baseURLComponents(
+        host: SKC_API_BASE_URL.description,
+        path: SKC_API_SEARCH_ENDPOINT.description
+    )
     components.queryItems = [
         URLQueryItem(name: "limit", value: "10"),
         URLQueryItem(name: "cName", value: cardName)
@@ -44,51 +51,46 @@ func searchCardURL(cardName: String) -> URL {
 }
 
 func dbStatsURL() -> URL {
-    var components = URLComponents()
-    components.scheme = "https"
-    components.host = SKC_API_BASE_URL.description
-    components.path = SKC_API_DB_STATS_ENDPOINT.description
-    
+    let components = baseURLComponents(
+        host: SKC_API_BASE_URL.description,
+        path: SKC_API_DB_STATS_ENDPOINT.description
+    )
     return createURL(components: components)
 }
 
 // SKC Suggestion Engine URL creation methods
 
 func cardSuggestionsURL(cardID: String) -> URL {
-    var components = URLComponents()
-    components.scheme = "https"
-    components.host = SKC_SUGGESTION_ENGINE_BASE_URL.description
-    components.path = String(format: SKC_SUGGESTION_ENGINE_CARD_SUGGESTIONS_ENDPOINT.description, cardID)
-    
+    let components = baseURLComponents(
+        host: SKC_SUGGESTION_ENGINE_BASE_URL.description,
+        path: String(format: SKC_SUGGESTION_ENGINE_CARD_SUGGESTIONS_ENDPOINT.description, cardID)
+    )
     return createURL(components: components)
 }
 
 func cardSupportURL(cardID: String) -> URL {
-    var components = URLComponents()
-    components.scheme = "https"
-    components.host = SKC_SUGGESTION_ENGINE_BASE_URL.description
-    components.path = String(format: SKC_SUGGESTION_ENGINE_CARD_SUPPORT_ENDPOINT.description, cardID)
-    
+    let components = baseURLComponents(
+        host: SKC_SUGGESTION_ENGINE_BASE_URL.description,
+        path: String(format: SKC_SUGGESTION_ENGINE_CARD_SUPPORT_ENDPOINT.description, cardID)
+    )
     return createURL(components: components)
 }
 
 func cardOfTheDayURL() -> URL {
-    var components = URLComponents()
-    components.scheme = "https"
-    components.host = SKC_SUGGESTION_ENGINE_BASE_URL.description
-    components.path = SKC_SUGGESTION_ENGINE_CARD_OF_THE_DAY_ENDPOINT.description
-    
+    let components = baseURLComponents(
+        host: SKC_SUGGESTION_ENGINE_BASE_URL.description,
+        path: SKC_SUGGESTION_ENGINE_CARD_OF_THE_DAY_ENDPOINT.description
+    )
     return createURL(components: components)
 }
 
 // Heart API URL creation methods
 
 func upcomingEventsURL() -> URL {
-    var components = URLComponents()
-    components.scheme = "https"
-    components.host = HEART_API_BASE_URL.description
-    components.path = HEART_API_EVENT_ENDPOINT.description
-    
+    var components = baseURLComponents(
+        host: HEART_API_BASE_URL.description,
+        path: HEART_API_EVENT_ENDPOINT.description
+    )
     components.queryItems = [
         URLQueryItem(name: "service", value: "skc"),
         URLQueryItem(name: "tags", value: "product-release")
