e22fcd0c437ad36c83e772e705a4be08105fe4cf
Javi <8519394+rtomyj@users.noreply.github.com>
2024-08-12 20:40:03 -0500

    Updated

--- SKCSwift/Views/Card/YGOCardView.swift
+++ SKCSwift/Views/Card/YGOCardView.swift
@@ -22,7 +22,7 @@ struct YGOCardView: View, Equatable {
         self.variant = variant
         
         self.width = (variant == .normal) ?  UIScreen.main.bounds.width : 220
-        self.imageSize = (variant == .normal) ? width - 60 : width
+        self.imageSize = (variant == .normal) ? width - 60 : width - 20
         self.imageSizeVariant = (variant == .normal) ? .medium : .extra_small
     }
     
