0a844d16ced320515116cc237a023f14cc60ce1e
Javi <8519394+rtomyj@users.noreply.github.com>
2024-11-09 16:11:55 -0600

    Updated view to handle all errors

--- SKCSwift/Views/Utility/NetworkErrorView.swift
+++ SKCSwift/Views/Utility/NetworkErrorView.swift
@@ -13,9 +13,13 @@ struct NetworkErrorView: View {
     
     private var description: String {
         return switch error {
-        case .server: "Network error occurred"
-        case .timeout: "Request timeout"
-        default: ""
+        case .server: "Server ran into an issue while processing your request"
+        case .timeout: "Request timed out"
+        case .client, .unknown: "Error occurred while fetching data"
+        case .badRequest, .unprocessableEntity: "Request could not be processed"
+        case .notFound: "Resource does not exist"
+        case .bodyParse: "Error decoding request"
+        case .cancelled: "Request was cancelled before completion"
         }
     }
     
@@ -23,7 +27,8 @@ struct NetworkErrorView: View {
         return switch error {
         case .server: "network.slash"
         case .timeout: "clock.badge.xmark"
-        default: ""
+        case .client, .badRequest, .unprocessableEntity, .bodyParse, .unknown: "exclamationmark.circle"
+        case .notFound, .cancelled: "nosign"
         }
     }
     
@@ -39,7 +44,7 @@ struct NetworkErrorView: View {
                 .buttonStyle(.borderedProminent)
             }
         default:
-            EmptyView()
+            ContentUnavailableView {Label(description, systemImage: icon)}
         }
     }
 }
@@ -48,6 +53,34 @@ struct NetworkErrorView: View {
     NetworkErrorView(error: .server, action: { print("Retried") })
 }
 
+#Preview("Client Error") {
+    NetworkErrorView(error: .client, action: { print("Retried") })
+}
+
+#Preview("Bad Request Error") {
+    NetworkErrorView(error: .badRequest, action: { print("Retried") })
+}
+
+#Preview("Unproccessible Entity Error") {
+    NetworkErrorView(error: .unprocessableEntity, action: { print("Retried") })
+}
+
+#Preview("Not Found Error") {
+    NetworkErrorView(error: .notFound, action: { print("Retried") })
+}
+
+#Preview("Body Parse Error") {
+    NetworkErrorView(error: .bodyParse, action: { print("Retried") })
+}
+
+#Preview("Cancelled") {
+    NetworkErrorView(error: .cancelled, action: { print("Retried") })
+}
+
 #Preview("Timeout") {
     NetworkErrorView(error: .timeout, action: { print("Retried") })
+}
+
+#Preview("Unknown") {
+    NetworkErrorView(error: .unknown, action: { print("Retried") })
 }
