61dfc60f02cb7732e700a2d98d8341144453d88c
Javi <8519394+rtomyj@users.noreply.github.com>
2025-02-09 15:30:01 -0600

    Added product metrics

--- SKCSwift/Views/Product/ProductView.swift
+++ SKCSwift/Views/Product/ProductView.swift
@@ -55,14 +55,29 @@ private struct ProductInfoView: View {
     let productID: String
     let product: Product?
     
+    @State private var showStats = false
+    
     var body: some View {
         VStack{
             ProductImageView(width: 150, productID: productID, imgSize: .small)
                 .padding(.vertical)
-            if let product = product {
+            if let product = product, let contents = product.productContent {
+                Text([product.productId, product.productType, product.productSubType].joined(separator: " | "))
+                    .font(.headline)
                 InlineDateView(date: product.productReleaseDate)
-                Text([product.productType, product.productSubType].joined(separator: " | "))
-                    .font(.subheadline)
+                
+                Button {
+                    showStats.toggle()
+                } label: {
+                    Label("Metrics", systemImage: "chart.bar.fill")
+                        .frame(width: 200)
+                }
+                .buttonStyle(.bordered)
+                .controlSize(.large)
+                .sheet(isPresented: $showStats, onDismiss: {showStats = false}) {
+                    ProductStatsView(productID: product.productId, productName: product.productName, rarities: contents.flatMap { $0.rarities }, cards: contents.compactMap { $0.card })
+                }
+                .padding(.bottom)
                 
                 if let contents = product.productContent {
                     ProductContentView(productID: productID, contents: contents)
@@ -74,6 +89,80 @@ private struct ProductInfoView: View {
         }
         .modifier(ParentViewModifier(alignment: .center))
         .padding(.bottom, 40)
+    }
+}
+
+private struct ProductStatsView: View {
+    let productID: String
+    let productName: String
+    
+    private let rarityData: [ChartData]
+    private let mstData: [ChartData]
+    private let monsterColorData: [ChartData]
+    private let monsterAttributeData: [ChartData]
+    
+    init(productID: String, productName: String, rarities: [String], cards: [Card]) {
+        self.productID = productID
+        self.productName = productName
+        
+        rarityData = rarities
+            .reduce(into: [String: Int]()) { counts, rarity in
+                counts[rarity.cardRarityShortHand(), default: 0] += 1
+            }
+            .map { ChartData(name: $0.key, count: $0.value) }
+        
+        mstData = cards
+            .reduce(into: [String: Int]()) { counts, card in
+                if card.attribute == .spell || card.attribute == .trap {
+                    counts[card.attribute.rawValue, default: 0] += 1
+                } else {
+                    counts["Monster", default: 0] += 1
+                }
+            }
+            .map { ChartData(name: $0.key, count: $0.value) }
+        
+        monsterColorData = cards
+           .filter { $0.attribute != .spell && $0.attribute != .trap }
+           .reduce(into: [String: Int]()) { counts, card in
+               counts[card.cardColor, default: 0] += 1
+           }
+           .map { ChartData(name: $0.key, count: $0.value) }
+        
+        monsterAttributeData = cards
+            .filter { $0.attribute != .spell && $0.attribute != .trap }
+            .reduce(into: [String: Int]()) { counts, card in
+                counts[card.cardAttribute!, default: 0] += 1
+            }
+            .map { ChartData(name: $0.key, count: $0.value) }
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Label {
+                    Text("Metrics")
+                        .font(.title)
+                } icon: {
+                    ProductImageView(width: 50, productID: productID, imgSize: .tiny)
+                }
+                .padding(.bottom)
+                
+                PieChartGroupView(
+                    description: "Distribution of all cards printed in **\(productName)** and their unique rarities. In some cases, cards might have multiple rarities (e.g. common, rare, etc).",
+                    dataTitle: "Rarity Distribution", data: rarityData)
+                PieChartGroupView(
+                    description: "Cards printed in **\(productName)** categorized into three main types: Monster, Spell, and Trap.",
+                    dataTitle: "M/S/T Distribution", data: mstData)
+                PieChartGroupView(
+                    description: "Monster cards printed in **\(productName)** categorized into their respective card color.",
+                    dataTitle: "Monster Color", data: monsterColorData)
+                PieChartGroupView(
+                    description: "Monster cards printed in **\(productName)** categorized by their attribute.",
+                    dataTitle: "Monster Attribute", data: monsterAttributeData)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.all)
+        }
     }
 }
 
