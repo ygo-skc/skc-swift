c0432a90ee39bf27272c98698a178335d23c2fa7
Javi <8519394+rtomyj@users.noreply.github.com>
2025-04-29 20:01:11 -0500

    Ensuring suggestion logic gets migrated to proper VM

--- SKCSwift/ViewModels/RecentlyViewedViewModel.swift
+++ SKCSwift/ViewModels/RecentlyViewedViewModel.swift
@@ -14,6 +14,7 @@ final class RecentlyViewedViewModel {
     private(set) var dataTaskStatus: DataTaskStatus = .uninitiated
     
     private(set) var recentlyViewedCardDetails = [Card]()
+    private(set) var recentlyViewedSuggestions: [CardReference] = []
     @ObservationIgnored
     private var recentlyViewedCardInfo = [String: Card]()
     
@@ -22,8 +23,12 @@ final class RecentlyViewedViewModel {
         
         requestError = nil
         dataTaskStatus = .pending
-        (recentlyViewedCardInfo, requestError) = await fetchRecentlyViewedDetails(newRecentlyViewed: recentlyViewedCardIDs,
-                                                                                                    recentlyViewedCardInfo: recentlyViewedCardInfo)
+        
+        async let detailsAsync = fetchRecentlyViewedDetails(newRecentlyViewed: recentlyViewedCardIDs, recentlyViewedCardInfo: recentlyViewedCardInfo)
+        async let suggestionAsync = fetchRecentlyViewedSuggestions(newlyViewed: recentlyViewedCardIDs)
+        
+        (recentlyViewedCardInfo, requestError) = await detailsAsync
+        recentlyViewedSuggestions = await suggestionAsync
         recentlyViewedCardDetails = newHistory.map{ recentlyViewedCardInfo[$0.id] }.compactMap{ $0 }
         dataTaskStatus = .done
     }
@@ -42,5 +47,44 @@ final class RecentlyViewedViewModel {
             }
         }
         return (recentlyViewedCardInfo, nil)
+    }
+    
+    nonisolated private func fetchRecentlyViewedSuggestions(newlyViewed: Set<String>) async  -> [CardReference] {
+        async let suggestionAsync = fetchRecentlyViewedSuggestionData(newlyViewed: newlyViewed)
+        async let supportAsync = fetchRecentlyViewedSupportData(newlyViewed: newlyViewed)
+
+        return await consolidateSuggestions(suggestions: await suggestionAsync, support: await supportAsync)
+    }
+
+    nonisolated private func fetchRecentlyViewedSuggestionData(newlyViewed: Set<String>) async -> BatchSuggestions {
+        switch await data(batchCardSuggestionsURL(), reqBody: BatchCardRequest(cardIDs: newlyViewed),
+                          resType: BatchSuggestions.self, httpMethod: "POST") {
+        case .success(let suggestions):
+            return suggestions
+        default: break
+        }
+        return BatchSuggestions(namedMaterials: [], namedReferences: [], materialArchetypes: Set(), referencedArchetypes: Set(),
+                                unknownResources: Set(), falsePositives: Set())
+    }
+
+    nonisolated private func fetchRecentlyViewedSupportData(newlyViewed: Set<String>) async -> BatchSupport {
+        switch await data(batchCardSupportURL(), reqBody: BatchCardRequest(cardIDs: newlyViewed),
+                          resType: BatchSupport.self, httpMethod: "POST") {
+        case .success(let suggestions):
+            return suggestions
+        default: break
+        }
+        return BatchSupport(referencedBy: [], materialFor: [], unknownResources: Set(), falsePositives: Set())
+    }
+
+    nonisolated private func consolidateSuggestions(suggestions: BatchSuggestions, support: BatchSupport) async -> [CardReference] {
+        let s = suggestions.namedMaterials + suggestions.namedReferences + support.materialFor + support.referencedBy
+        return Array(s
+            .reduce(into: [String: CardReference]()) { accumulator, ref in
+                accumulator[ref.card.cardID] = CardReference(occurrences: accumulator[ref.card.cardID]?.occurrences ?? 0 + ref.occurrences, card: ref.card)
+            }
+            .values
+            .sorted(by: { $0.occurrences > $1.occurrences })
+            .prefix(8))
     }
 }
