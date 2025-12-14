b78b58d9a5d903a1831ba5a1af889d8216af4ee4
Javi <8519394+rtomyj@users.noreply.github.com>
2025-12-14 00:23:38 -0600

    Updated color

--- SKCSwift/Views/Card/YGOCardArchetypesView.swift
+++ SKCSwift/Views/Card/YGOCardArchetypesView.swift
@@ -29,7 +29,7 @@ struct YGOCardArchetypesView: View {
                                 selectedArchetype = archetype
                             }
                             .buttonStyle(.borderedProminent)
-                            .tint(.container)
+                            .tint(.blueGray)
                         }
                     }
                 }
