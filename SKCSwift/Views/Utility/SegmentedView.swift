8a188c561c726287c74b59b3853390a72c19852d
Javi <8519394+rtomyj@users.noreply.github.com>
2026-03-15 16:19:04 -0500

    Removed old unused code

--- SKCSwift/Views/Utility/SegmentedView.swift
+++ SKCSwift/Views/Utility/SegmentedView.swift
@@ -22,110 +22,48 @@ struct SegmentedView<MainContent: View, SheetContent: View>: View {
     
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
-            GeometryReader { reader in
-                let frame = reader.frame(in: .global)
-                mainContent
-                BottomSheet(frame: frame, mainSheetContentHeight: $mainSheetContentHeight) {
-                    sheetContent
-                }
+            mainContent
+            BottomSheet(mainSheetContentHeight: $mainSheetContentHeight) {
+                sheetContent
             }
         }
     }
 }
 
 private struct BottomSheet<SheetContent: View>: View {
-    let frameHeight: CGFloat
-    let frameMidX: CGFloat
     @Binding var mainSheetContentHeight: CGFloat
     let content: SheetContent
     
-    @State private var offset: CGFloat = 0
-    @State private var bottomSheetHeight: CGFloat = 0
-    
-    init(frame: CGRect, mainSheetContentHeight: Binding<CGFloat>, @ViewBuilder content: () -> SheetContent) {
-        self.frameHeight = frame.height
-        self.frameMidX = frame.midX
+    init(mainSheetContentHeight: Binding<CGFloat>, @ViewBuilder content: () -> SheetContent) {
         self._mainSheetContentHeight = mainSheetContentHeight
         self.content = content()
     }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            VStack {
-//                Capsule()
-//                    .fill(.gray.opacity(0.7))
-//                    .frame(width: 40, height: 5)
-//                    .padding([.top, .bottom], 5)
-                content
-                    .padding([.top, .bottom])
-            }
-            .background(GeometryReader { geometry in
-                Color.clear.preference(
-                    key: BottomSheetMainContentHeightPreferenceKey.self,
-                    value: geometry.size.height
-                )
-            })
-            .onPreferenceChange(BottomSheetMainContentHeightPreferenceKey.self) { [$mainSheetContentHeight] newValue in
-                $mainSheetContentHeight.wrappedValue = newValue
-            }
-        }
-        .background(GeometryReader { geometry in
-            Color.clear.preference(
-                key: BottomSheetMinHeightPreferenceKey.self,
-                value: geometry.size.height
-            )
-        })
-        .onPreferenceChange(BottomSheetMinHeightPreferenceKey.self) { [$bottomSheetHeight] newValue in
-            $bottomSheetHeight.wrappedValue = newValue
+            content
+                .padding([.top, .bottom])
+                .background(GeometryReader { geometry in
+                    Color.clear.preference(
+                        key: BottomSheetMinHeightPreferenceKey.self,
+                        value: geometry.size.height
+                    )
+                })
+                .onPreferenceChange(BottomSheetMinHeightPreferenceKey.self) { [$mainSheetContentHeight] newValue in
+                    $mainSheetContentHeight.wrappedValue = newValue
+                }
         }
         .padding(.horizontal)
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .frame(maxWidth: .infinity, minHeight: mainSheetContentHeight + 50, alignment: .topLeading)
         .background(BlurView(style: .systemMaterial))
-        .cornerRadius(15)
-        .shadow(radius: 3)
+        .cornerRadius(12)
+        .shadow(radius: 4)
         .ignoresSafeArea(.all, edges: .bottom)
-        .offset(y: frameHeight - bottomSheetHeight + offset)
-//        .gesture(DragGesture().onChanged { value in
-//            if value.startLocation.y > frameMidX {
-//                if value.translation.height < 0 && offset > (-frameHeight + bottomSheetHeight) {
-//                    offset = value.translation.height
-//                }
-//            } else if value.startLocation.y < frameMidX {
-//                if value.translation.height > 0 && offset < 0 {
-//                    offset = (-frameHeight + bottomSheetHeight) + value.translation.height
-//                }
-//            }
-//        }
-//            .onEnded{ value in
-//                withAnimation(.bouncy(duration: 0.1, extraBounce: 0.1)) {
-//                    if value.startLocation.y > frameMidX {
-//                        if -value.translation.height > frameMidX {
-//                            offset = (-frameHeight) + bottomSheetHeight
-//                        } else {
-//                            offset = 0
-//                        }
-//                    } else if value.startLocation.y < frameMidX {
-//                        if value.translation.height < frameMidX {
-//                            offset = (-frameHeight) + bottomSheetHeight
-//                        } else {
-//                            offset = 0
-//                        }
-//                    }
-//                }
-//            }
-//        )
+        .offset(y: 50)
     }
 }
 
 private struct BottomSheetMinHeightPreferenceKey: PreferenceKey {
-    static let defaultValue: CGFloat = 0
-    
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = max(value, nextValue())
-    }
-}
-
-private struct BottomSheetMainContentHeightPreferenceKey: PreferenceKey {
     static let defaultValue: CGFloat = 0
     
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
