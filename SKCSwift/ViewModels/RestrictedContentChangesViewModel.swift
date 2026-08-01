b2dfa4b2be17af2cd2f9f23323a6265ba383bec2
Javi <8519394+rtomyj@users.noreply.github.com>
2026-08-01 01:25:20 -0500

    copy pasta fix

--- SKCSwift/ViewModels/RestrictedContentChangesViewModel.swift
+++ SKCSwift/ViewModels/RestrictedContentChangesViewModel.swift
@@ -53,7 +53,8 @@ final class RestrictedContentChangesViewModel {
     }
     
     private func fetchRemovedContent() async {
-        if newContentDTS == .done { return }
+        if removedContentDTS == .done { return }
+        (removedContentNE, removedContentDTS) = (nil, .pending)
         let res = await SKCSwift.data(removedBannedContent(format: format, listStartDate: effectiveDate), resType: BanListRemovedContent.self)
         if case .success(let removedContent) = res {
             self.removedContent = removedContent
