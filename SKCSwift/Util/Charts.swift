353cb75081d47a2b728c591716630f637c7c7c9c
Javi <8519394+rtomyj@users.noreply.github.com>
2026-08-01 22:56:25 -0500

    Making color map immutable

--- SKCSwift/Util/Charts.swift
+++ SKCSwift/Util/Charts.swift
@@ -31,22 +31,20 @@ struct Charts {
     static let colors: [Color] = [.red, .orange, .yellow, .green, .blue, .purple, .pink, .brown,
                                   .cyan, .mint, .indigo, .teal, .synchroYGOCard, .dateRed, .effectYGOCard, .normalYGOCard, .fusionYGOCard]
     
-    private static var colorCache: [String: Color] = ["Monster": .normalYGOCard, "Spell": .spellYGOCard, "Trap": .trapYGOCard,
+    private static let seededColors: [String: Color] = ["Monster": .normalYGOCard, "Spell": .spellYGOCard, "Trap": .trapYGOCard,
                                        "Dark": .purple, "Light": .yellow, "Wind": .green, "Water": .blue, "Fire": .red, "Earth": .brown,
                                        "Normal": .normalYGOCard, "Effect": .effectYGOCard, "Ritual": .ritualYGOCard, "Fusion": .fusionYGOCard,
                                        "Synchro": .synchroYGOCard, "Xyz": .xyzYGOCard, "Link": .linkYGOCard, "Pendulum Normal": .normalYGOCard,
                                        "Pendulum Effect": .effectYGOCard, "Pendulum Fusion": .fusionYGOCard, "Pendulum Synchro": .synchroYGOCard,
                                        "Pendulum Xyz": .xyzYGOCard]
-    
+
     static func determineChartColor(_ category: String) -> Color {
-        if let cachedColor = colorCache[category] {
-            return cachedColor
+        if let seededColor = seededColors[category] {
+            return seededColor
         }
         let hashValue = abs(category.hash) % 1000
         let index = hashValue % Charts.colors.count
-        let color = Charts.colors[index]
-        colorCache[category] = color
-        return color
+        return Charts.colors[index]
     }
     
     static func sortChart(_ lhs: ChartData, _ rhs: ChartData) -> Bool {
